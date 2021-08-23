b35a7715a0000cd879720f234c1324778cbe327a
Johan Groenen <jgroenen@jgroenen.nl>
2021-08-23 10:09:26 +0200

    to git

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href='css/reset.css' rel='stylesheet' type='text/css'/>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,700,300' rel='stylesheet' type='text/css'>
+		<style>
+			body {
+				font-family: "Roboto Condensed", Helvetica, Arial, sans-serif;
+				font-size: 100%;
+				color: #666;
+			}
+			article {
+				width: 800px;
+				margin: 80px auto;
+				clear: both;
+			}
+			h1 {
+				font-size: 2.2em;
+				color: #3d8;
+			}
+			.quote {
+			}
+			.quote h2 {
+				text-align: center;
+				font-size: 2.6em;
+				color: #3d8;
+				line-height: 1.4em;
+			}
+			.quote h3 {
+				font-size: 1.4em;
+				float: right;
+				margin-top: 20px;
+				margin-right: 40px;
+			}
+			.logo {
+				margin-top: 200px;
+				text-align: center;
+			}
+			.logo h2 {
+				margin-top: 20px;
+			}
+			.link p {
+				text-align: center;
+				line-height: 1.4;
+			}
+			footer {
+				width: 800px;
+				margin: 40px auto;
+				text-align: center;
+				font-size: 0.8em;
+				font-weight: 300;
+			}
+			footer a {
+				color: #666;
+				text-decoration: none;
+			}
+			.examples {
+				margin-top: 200px;
+			    text-align: center;
+			}
+			.example {
+			    display: inline-block;
+			    width: 300px;
+			}
+			.examples a {
+			    color: #000;
+			    text-decoration: none;
+			}
+			.example img {
+			    width: 80%;
+			    margin: 20px auto;
+			    margin-left: 10px;
+			}
+			.example h2 {
+			    font-size: 1.8em;
+			    line-height: 2em;
+			}
+			.example .body {
+			    padding: 10px;
+			    padding-bottom: 1em;
+			    border-bottom: 1px solid gray;
+			}
+		</style>
+	</head>
+	<body>
+		<article class="quote">
+		    <h2>Herstel en behoud van locale ecosystemen. Natuurlijke, sociale en economische. Een grassroots beweging voor schaalverkleining.</h2>
+		</article>
+		<article class="examples">
+		    <a href="http://www.herenboeren.nl/">
+		        <div class="example">
+		            <div class="header" style="background: #000">
+		                <img src="http://www.herenboeren.nl/static/_assets/img/payoff.png">
+		            </div>
+		            <div class="body">
+		                <h2>Herenboeren</h2>
+		                <p>Samen duurzaam voedsel produeren</p>
+		            </div>
+		        </div>
+		    </a>
+		</article>
+		<article class="logo">
+			<h1>Rescale EU</h1>
+			<h2>Preserving local ecosystems in a globalizing world.</h2>
+		</article>
+		<article class="link">
+		    <p>Volg onze <a href="https://twitter.com/rescaleeu">Twitter stream</a> voor idee&euml;n, informatie en meer.</p>
+		</article>
+		<footer>
+			<p>Of stuur een e-mail naar <a href="mailto:info@rescale.eu">info@rescale.eu</a>.</p>
+		</footer>
+	</body>
+</html>
